9c84d88e536ce425bfb5538b8b47ae2e7ff70c4f
kelmen <kelmen@hotmail.com>
2019-09-18 04:06:07 +0800

    Kelmen.ONI.Mods.StorageBins

---
+++ Kelmen.ONI.Mods.StorageBins/EfficientSmartStorageBin.cs
@@ -0,0 +1,56 @@
+﻿using KSerialization;
+using UnityEngine;
+
+using Kelmen.ONI.Mods.Shared.CaiLib.Utils;
+using static Kelmen.ONI.Mods.Shared.CaiLib.Utils.BuildingUtils;
+using static Kelmen.ONI.Mods.Shared.CaiLib.Utils.StringUtils;
+using TUNING;
+
+namespace Kelmen.ONI.Mods.StorageBins
+{
+    [SerializationConfig(MemberSerialization.OptIn)]
+    public class EfficientSmartStorageBin : StorageLockerSmartConfig
+    {
+        public new const string ID = "Kelmen-EfficientSmartStorageBin";
+
+        public const string DisplayName = "Efficient Smart Storage Bin";
+        public const string Description = "Work like vanilla smart storage bin, but remove power and heat.";
+        public const string Effect = "Send out true signal when defined maximum storage is meet.";
+
+        public override BuildingDef CreateBuildingDef()
+        {
+            var buildingDef = base.CreateBuildingDef();
+
+            buildingDef.PrefabID = ID;
+            buildingDef.RequiresPowerInput = false;
+            buildingDef.EnergyConsumptionWhenActive = 0;
+            buildingDef.ExhaustKilowattsWhenActive = 0;
+            //buildingDef.SelfHeatKilowattsWhenActive = 0;
+
+            buildingDef.InitDef();
+
+            return buildingDef;
+        }
+
+        public static void SetDescriptions()
+        {
+            AddBuildingStrings(ID, DisplayName, Description, Effect);
+        }
+
+        public static void SetMenu()
+        {
+            AddBuildingToPlanScreen(GameStrings.PlanMenuCategory.Base, ID);
+        }
+
+        public static void SetTechTree()
+        {
+            AddBuildingToTechnology(GameStrings.Technology.SolidMaterial.SmartStorage, ID);
+        }
+
+        public static Color32 ChangeColor()
+        {
+            return new Color32(128, 255, 128, 255);
+        }
+
+    }
+}
